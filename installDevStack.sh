dd68bf983deba15cce5830ebd9e6de7b2781d8e4
xoze <87602718+x0ze@users.noreply.github.com>
2025-01-06 13:32:55 +0100

    update script

--- installDevStack.sh
+++ installDevStack.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
+
+# Add User
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 sudo chmod +x /opt/stack
+
+# Add right for user
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
-sudo -u stack -i
-git clone https://opendev.org/openstack/devstack
-cd devstack
-touch local.conf
-echo "[[local|localrc]]
-ADMIN_PASSWORD=secret
-DATABASE_PASSWORD=$ADMIN_PASSWORD
-RABBIT_PASSWORD=$ADMIN_PASSWORD
-SERVICE_PASSWORD=$ADMIN_PASSWORD" > local.conf
+
+sudo -u stack -i <<EOF
+  git clone https://opendev.org/openstack/devstack
+  cd devstack
+  touch local.conf
+  echo "[[local|localrc]]" > local.conf
+  echo "ADMIN_PASSWORD=secret" >> local.conf
+  echo "DATABASE_PASSWORD=\$ADMIN_PASSWORD" >> local.conf
+  echo "RABBIT_PASSWORD=\$ADMIN_PASSWORD" >> local.conf
+  echo "SERVICE_PASSWORD=\$ADMIN_PASSWORD" >> local.conf
+EOF
